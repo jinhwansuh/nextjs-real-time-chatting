baac9e187479079d0cef63238a1fc5fec40014cb
jinhwansuh <nick02190219@gmail.com>
2022-06-14 02:10:27 +0900

    feat(server): 클라이언트와 서버 연결

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,15 @@
+import { Request, Response } from 'express';
+
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://example.com',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('<h1>test</h1>');
 });
 
@@ -15,6 +17,6 @@ io.on('connection', () => {
   console.log('a user connected');
 });
 
-server.listen(3000, () => {
-  console.log('listening chat project, port: 3000');
+server.listen(8000, () => {
+  console.log('listening chat project, port: 8000');
 });
