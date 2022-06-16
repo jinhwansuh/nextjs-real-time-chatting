f96cbfc5308f6eba0ece25461aed500b94926722
jinhwansuh <nick02190219@gmail.com>
2022-06-16 18:24:20 +0900

    feat(server): 실시간 이용자 인원 구현

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import http from 'http';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
@@ -21,10 +21,41 @@ app.get('/', (req: express.Request, res: express.Response) => {
   res.send('<h1>test</h1>');
 });
 
-io.on('connection', (socket: any) => {
-  console.log('a user connected');
+// const chatRoom1 = io.of('/room1');
+// chatRoom1.on('connection', (socket: Socket) => {
+//   console.log('room1: a user connected');
+//   socket.on('chat message', (message: Message) => {
+//     io.emit('send message', message);
+//   });
+// });
+
+// const chatRoom2 = io.of('/room2');
+// chatRoom2.on('connection', (socket: Socket) => {
+//   console.log('room2: a user connected');
+//   socket.on('chat message', (message: Message) => {
+//     io.emit('send message', message);
+//   });
+// });
+
+const user = new Set();
+
+io.on('connection', (socket: Socket) => {
+  console.log('someone connected Index');
+
+  user.add(socket.id);
+
+  io.emit('welcome', {
+    time: [...user],
+  });
+
   socket.on('chat message', (message: Message) => {
     io.emit('send message', message);
+  });
+
+  socket.on('disconnect', () => {
+    const disconnectedUser = socket.id;
+    user.delete(disconnectedUser);
+    io.emit('bye', {});
   });
 });
 
