32381f6b9ef71c76baacb509516e4197750fa87a
jinhwansuh <nick02190219@gmail.com>
2022-06-19 23:48:43 +0900

    refactor(client): socket 연결 useEffect로 변경

--- client/src/pages/chat/index.tsx
+++ client/src/pages/chat/index.tsx
@@ -1,13 +1,24 @@
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
 
 import { RoomList, ChattingArea, Main } from './index.styled';
 
 const Chat: NextPage = () => {
+  const [currentSocket, setCurrentSocket] = useState<Socket>();
+
+  useEffect(() => {
+    const socket = io(`http://localhost:8000`, {
+      transports: ['websocket'],
+    });
+
+    setCurrentSocket(socket);
+  }, []);
+
   return (
     <Main>
-      <RoomList />
-      <ChattingArea />
+      <RoomList socket={currentSocket} />
+      <ChattingArea socket={currentSocket} />
     </Main>
   );
 };
