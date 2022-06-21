f9a44e61cd14f7f46083c7dd51a385c1aacbe2ab
jinhwansuh <nick02190219@gmail.com>
2022-06-22 00:31:35 +0900

    refactor(client): 사용자 이름 받는 폼 refactoring

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -1,31 +1,22 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { useRecoilState } from 'recoil';
-import { user, userNameModal } from '../../atoms/user';
+import { user } from '../../atoms/user';
 
 export default function Layout({ children }: any) {
   const [userState, setUserState] = useRecoilState(user);
-  const [userNameModalState, setUserNameModalState] =
-    useRecoilState(userNameModal);
   const [userName, setUserName] = useState('');
 
-  useEffect(() => {
-    if (userState.name.length === 0) {
-      setUserNameModalState(true);
-    }
-  }, [userState.name]);
-
-  const handleUserModal = (e: FormEvent<HTMLFormElement>) => {
+  const handleUserName = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setUserState({ name: userName });
-    setUserNameModalState(false);
     setUserName('');
   };
 
   if (userState.name.length === 0)
     return (
-      <div style={{ display: userNameModalState ? 'block' : 'none' }}>
-        <form onSubmit={handleUserModal}>
-          이름을 입력해주세요.
+      <div>
+        <form onSubmit={handleUserName}>
+          이름을 입력해주세요. ~~
           <div>
             <input
               value={userName}
