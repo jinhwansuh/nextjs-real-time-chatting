3223710d603fa2d9430bf539b62aea5d030d00dc
jinhwansuh <nick02190219@gmail.com>
2022-06-24 16:27:08 +0900

    feat(client): user에 socketId 타입 추가

--- client/src/atoms/user.ts
+++ client/src/atoms/user.ts
@@ -1,8 +1,10 @@
 import { atom } from 'recoil';
+import { UserAtom } from '../types/chat';
 
-export const user = atom({
+export const user = atom<UserAtom>({
   key: 'userStateKey',
   default: {
     name: '',
+    userSocketId: '',
   },
 });
