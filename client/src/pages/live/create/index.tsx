317ded1bc20601dcc8ebdb25ccd791bb97c0c71b
jinhwansuh <nick02190219@gmail.com>
2022-06-26 16:21:28 +0900

    feat(client): 내 화면 공유 추가

--- client/src/pages/live/create/index.tsx
+++ client/src/pages/live/create/index.tsx
@@ -6,35 +6,50 @@ const Create = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
+    console.log(streamState, videoRef);
     if (videoRef.current)
       videoRef.current.srcObject = streamState ? streamState : null;
   }, [streamState]);
 
-  const handleClick = async (e: MouseEvent<HTMLButtonElement>) => {
+  const handleVideoClick = async (e: MouseEvent<HTMLButtonElement>) => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
+        audio: false,
         video: true,
       });
-      setStreamState(() => stream);
+      setStreamState(stream);
 
       e.currentTarget.disabled = true;
     } catch (e) {
       // handleError(e);
     }
   };
+  const handleDisplayClick = async (e: MouseEvent<HTMLButtonElement>) => {
+    try {
+      const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+      });
+      setStreamState(stream);
 
+      e.currentTarget.disabled = true;
+    } catch (e) {
+      // handleError(e);
+    }
+  };
   return (
     <>
       <StyledVideo ref={videoRef} autoPlay />
-      <button onClick={handleClick}>열기</button>
+      <div>
+        <button onClick={handleVideoClick}>비디오 연결하기</button>
+        <button onClick={handleDisplayClick}>화면 공유하기</button>
+      </div>
       <div>방송 만들기</div>
     </>
   );
 };
 
 const StyledVideo = styled.video`
-  width: 50%;
+  width: 80%;
   height: 400px;
 `;
 
