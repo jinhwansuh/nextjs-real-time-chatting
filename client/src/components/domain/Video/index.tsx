5b1f646a60b3ce89dd6413b40c9c3cca80dc0d95
jinhwansuh <nick02190219@gmail.com>
2022-06-29 16:09:44 +0900

    feat(client): Video 컴포넌트 구현

---
+++ client/src/components/domain/Video/index.tsx
@@ -0,0 +1,35 @@
+import { CSSProperties, RefObject } from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  videoRef: RefObject<HTMLVideoElement>;
+  autoPlay?: boolean;
+  playsInline?: boolean;
+  muted?: boolean;
+  style?: CSSProperties;
+}
+
+const Video = ({
+  videoRef,
+  autoPlay = true,
+  playsInline,
+  muted,
+  ...props
+}: Props) => {
+  return (
+    <StyledVideo
+      {...props}
+      ref={videoRef}
+      autoPlay={autoPlay}
+      playsInline={playsInline}
+      muted={muted}
+    />
+  );
+};
+
+const StyledVideo = styled.video`
+  width: 80%;
+  height: 400px;
+`;
+
+export default Video;
