9961a49e55e7da6bdc9fc9abdee52275732b03b5
jinhwansuh <nick02190219@gmail.com>
2022-07-01 15:13:17 +0900

    feat(client): 방송을 보며 채팅을 가능하도록 구현

--- client/src/pages/live/[roomId].tsx
+++ client/src/pages/live/[roomId].tsx
@@ -1,13 +1,21 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { useEffect, useRef, useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 import { io, Socket } from 'socket.io-client';
+import styled from 'styled-components';
+import { v4 } from 'uuid';
+import { user } from '../../atoms/user';
 import { Video } from '../../components/domain';
+import { Message } from '../../types/chat';
 import { VideoEventActions } from '../../types/constants';
 
 const StreamingRoom: NextPage = () => {
   const router = useRouter();
+  const userState = useRecoilValue(user);
   const [currentSocket, setCurrentSocket] = useState<Socket>();
+  const [chatInputState, setChatInputState] = useState('');
+  const [chatListState, setChatListState] = useState<Message[]>([]);
   const videoRef = useRef<HTMLVideoElement>(null);
   const { roomId } = router.query;
 
@@ -22,10 +30,28 @@ const StreamingRoom: NextPage = () => {
     const socket = io(`http://localhost:8000/streaming`);
 
     socket.on('connect', () => {
+      /* 
+        여기서 enter_room을 emit 한다.
+        서버에서 on(enter_room)을 받고 emit(Chat)을 한다.
+
+      */
       socket.emit(VideoEventActions.WATCHER, { roomId });
+      socket.emit(VideoEventActions.ENTER_ROOM, { roomId }); // 보내주기만 하면 되지 않을까?
     });
 
     setCurrentSocket(socket);
+
+    socket.on(VideoEventActions.CHAT_MESSAGE, (data: Message) => {
+      setChatListState((prev) => [
+        ...prev,
+        {
+          userSocketId: data.userSocketId,
+          name: data.name,
+          roomId: data.roomId,
+          message: data.message,
+        },
+      ]);
+    });
 
     socket.on(VideoEventActions.OFFER, (id, description) => {
       peerConnection = new RTCPeerConnection(config);
@@ -68,14 +94,56 @@ const StreamingRoom: NextPage = () => {
     };
   }, [roomId]);
 
+  const handleChatSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data: Message = {
+      name: userState.name,
+      roomId: roomId as string,
+      message: chatInputState,
+      userSocketId: userState.userSocketId,
+    };
+    currentSocket?.emit(VideoEventActions.CHAT_MESSAGE, data);
+    setChatInputState('');
+  };
+  //
   return (
     <>
-      <div>스트리밍</div>
+      <h1>스트리밍</h1>
       <div>
         <Video videoRef={videoRef} autoPlay playsInline muted />
       </div>
+      <StyledChattingContainer>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+        soluta vitae tenetur tempore accusantium impedit magnam voluptatum rem.
+        Officiis vero qui ut quas, illo officia eius reiciendis repellat aut
+        nesciunt.
+        {chatListState.map((chat) => (
+          <StyledChatItem key={v4()}>
+            {chat.name} : {chat.message}
+          </StyledChatItem>
+        ))}
+        <form onSubmit={handleChatSubmit}>
+          <input
+            value={chatInputState}
+            onChange={(e) => setChatInputState(e.target.value)}
+          />
+          <button type="submit" disabled={!chatInputState.trim().length}>
+            전송
+          </button>
+        </form>
+      </StyledChattingContainer>
     </>
   );
 };
 
+const StyledChattingContainer = styled.div`
+  overflow-y: auto;
+  background-color: #eee;
+  width: 80%;
+  height: 250px;
+`;
+
+const StyledChatItem = styled.div`
+  background-color: #ababef;
+`;
 export default StreamingRoom;
