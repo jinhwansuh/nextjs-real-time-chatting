624f5eb77afb77c6413c76831d297a6f34dd95d2
jinhwansuh <nick02190219@gmail.com>
2022-07-01 21:27:49 +0900

    design(client): 방 디자인 변경

--- client/src/components/domain/RoomList/index.tsx
+++ client/src/components/domain/RoomList/index.tsx
@@ -10,29 +10,32 @@ const RoomList = ({
 }: RoomListProps) => {
   if (!serverData) {
     return (
-      <StyledWrapper {...props}>
+      <StyledContainer {...props}>
         <div>소켓 연결에 실패했습니다.</div>
-      </StyledWrapper>
+      </StyledContainer>
     );
   }
   return (
-    <StyledWrapper {...props}>
-      <div>전체 인원 : {serverData.allUserCount}</div>
-      {/* <div>새로고침</div> */}
-      {roomState.name === '' ? (
-        <div>
-          <strong>방을 선택해주세요</strong>
-        </div>
-      ) : (
-        <div>
-          <strong>{roomState.name}</strong> 방 인원{' '}
-          <strong>{clientInRoom}</strong>
-        </div>
-      )}
-      <div>
+    <StyledContainer {...props}>
+      <StyledTitleWrapper>
+        <div>전체 인원 : {serverData.allUserCount}</div>
+
+        {roomState.name === '' ? (
+          <div>
+            <strong>방을 선택해주세요</strong>
+          </div>
+        ) : (
+          <div>
+            <strong>{roomState.name}</strong> 방 인원{' '}
+            <strong>{clientInRoom}</strong>
+          </div>
+        )}
+        <button>새로고침</button>
+      </StyledTitleWrapper>
+      <StyledRoomWrapper>
         {serverData.createdRoom.map((room) => (
           <div key={room._id}>
-            <StyledButton
+            <StyledRoom
               onClick={() =>
                 handleRoomChange({
                   id: room._id,
@@ -41,16 +44,16 @@ const RoomList = ({
               }
             >
               {room.roomName}
-            </StyledButton>
+            </StyledRoom>
           </div>
         ))}
-      </div>
-      {/* <div>새로운 방 생성하기</div> */}
-    </StyledWrapper>
+      </StyledRoomWrapper>
+      <div>새로운 방 생성하기</div>
+    </StyledContainer>
   );
 };
 
-const StyledWrapper = styled.div`
+const StyledContainer = styled.div`
   overflow-y: auto;
   overflow-x: hidden;
   box-sizing: border-box;
@@ -65,16 +68,27 @@ const StyledWrapper = styled.div`
     background-color: #eee;
   }
 `;
+const StyledTitleWrapper = styled.div`
+  background-color: #70b4f4;
+  min-height: 130px;
+`;
 
-const StyledButton = styled.button`
-  margin-bottom: 20px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+const StyledRoomWrapper = styled.div`
+  background-color: #ffffff;
+  margin-top: 20px;
+`;
+
+const StyledRoom = styled.div`
   padding: 4px;
-  margin: 6px;
-  width: calc(100% - 12px);
+  margin-bottom: 10px;
+  width: 100%;
+  height: 40px;
+  border: 1px solid black;
   box-sizing: border-box;
   cursor: pointer;
+  &:hover {
+    background-color: #c6c6f3;
+  }
 `;
 
 export default RoomList;
