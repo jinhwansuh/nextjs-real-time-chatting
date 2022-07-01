60f6788b76817c98786ce80bef4bb529e62b45ab
jinhwansuh <nick02190219@gmail.com>
2022-07-01 16:24:39 +0900

    refactor(client): StreamingChattingArea 컴포넌트 적용

--- client/src/pages/live/[roomId].tsx
+++ client/src/pages/live/[roomId].tsx
@@ -1,12 +1,11 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { FormEvent, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { io, Socket } from 'socket.io-client';
 import styled from 'styled-components';
-import { v4 } from 'uuid';
 import { user } from '../../atoms/user';
-import { Video } from '../../components/domain';
+import { StreamingChattingArea, Video } from '../../components/domain';
 import { Message } from '../../types/chat';
 import { VideoEventActions } from '../../types/constants';
 
@@ -14,10 +13,8 @@ const StreamingRoom: NextPage = () => {
   const router = useRouter();
   const userState = useRecoilValue(user);
   const [currentSocket, setCurrentSocket] = useState<Socket>();
-  const [chatInputState, setChatInputState] = useState('');
   const [chatListState, setChatListState] = useState<Message[]>([]);
   const videoRef = useRef<HTMLVideoElement>(null);
-  const chattingRef = useRef<HTMLDivElement>(null);
   const { roomId } = router.query;
 
   useEffect(() => {
@@ -99,21 +96,6 @@ const StreamingRoom: NextPage = () => {
     };
   }, [roomId]);
 
-  useEffect(() => {
-    chattingRef.current?.scrollTo(0, chattingRef.current.scrollHeight);
-  }, [chatListState]);
-
-  const handleChatSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const data: Message = {
-      name: userState.name,
-      roomId: roomId as string,
-      message: chatInputState,
-      userSocketId: userState.userSocketId,
-    };
-    currentSocket?.emit(VideoEventActions.CHAT_MESSAGE, data);
-    setChatInputState('');
-  };
   //
   return (
     <>
@@ -121,50 +103,13 @@ const StreamingRoom: NextPage = () => {
       <div>
         <Video videoRef={videoRef} autoPlay playsInline muted />
       </div>
-      <StyledChattingContainer ref={chattingRef}>
-        <StyledChattingWrapper>
-          {chatListState.map((chat) => (
-            <StyledChatItem key={v4()}>
-              {chat.name} : {chat.message}
-            </StyledChatItem>
-          ))}
-        </StyledChattingWrapper>
-        <StyledForm onSubmit={handleChatSubmit}>
-          <input
-            value={chatInputState}
-            onChange={(e) => setChatInputState(e.target.value)}
-          />
-          <button type="submit" disabled={!chatInputState.trim().length}>
-            전송
-          </button>
-        </StyledForm>
-      </StyledChattingContainer>
+      <StreamingChattingArea
+        chatListState={chatListState}
+        roomId={roomId as string}
+        currentSocket={currentSocket}
+      ></StreamingChattingArea>
     </>
   );
 };
 
-const StyledChattingContainer = styled.div`
-  overflow-y: auto;
-  position: relative;
-  background-color: #eee;
-  width: 80%;
-  height: 200px;
-`;
-const StyledChattingWrapper = styled.div`
-  min-height: calc(100% - 25px);
-`;
-const StyledForm = styled.form`
-  position: sticky;
-  bottom: 0px;
-  width: 100%;
-  display: flex;
-  height: 25px;
-  background-color: #ffffff;
-  /* padding: 6px; */
-  box-sizing: border-box;
-`;
-
-const StyledChatItem = styled.div`
-  background-color: #ababef;
-`;
 export default StreamingRoom;
