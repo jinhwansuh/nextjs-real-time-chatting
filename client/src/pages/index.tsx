31578d6e5388cbbe16aab4b68b805e1364212082
jinhwansuh <nick02190219@gmail.com>
2022-07-28 18:41:31 +0900

    rename(client): 사용안하는 구문 제거

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import { ReactElement, useState } from 'react';
 import styled from 'styled-components';
-import Layout from '../components/layout';
-import type { NextPageWithLayout } from './_app';
 
 const Home: NextPage = () => {
   return (
