6c7c090c16fd248ea2720b810d08a5ec3855134d
jinhwansuh <nick02190219@gmail.com>
2022-08-05 17:44:20 +0900

    design(client): Video 넓이 변경, prop으로 넓이 배경색을 받도록 설정

--- client/src/components/domain/Video/index.tsx
+++ client/src/components/domain/Video/index.tsx
@@ -2,10 +2,13 @@ import { CSSProperties, RefObject } from 'react';
 import styled from 'styled-components';
 
 interface Props {
+  width: string;
+  height: string;
   videoRef: RefObject<HTMLVideoElement>;
   autoPlay?: boolean;
   playsInline?: boolean;
   muted?: boolean;
+  backColor?: string;
   style?: CSSProperties;
 }
 
@@ -17,19 +20,28 @@ const Video = ({
   ...props
 }: Props) => {
   return (
-    <StyledVideo
-      {...props}
-      ref={videoRef}
-      autoPlay={autoPlay}
-      playsInline={playsInline}
-      muted={muted}
-    />
+    <StyledVideoWrapper {...props}>
+      <StyledVideo
+        ref={videoRef}
+        autoPlay={autoPlay}
+        playsInline={playsInline}
+        muted={muted}
+      />
+    </StyledVideoWrapper>
   );
 };
 
+const StyledVideoWrapper = styled.div<
+  Pick<Props, 'width' | 'height' | 'backColor'>
+>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  background-color: ${(props) => props.backColor || '#ccc'};
+`;
+
 const StyledVideo = styled.video`
-  width: 500px;
-  height: 400px;
+  width: 100%;
+  height: 100%;
 `;
 
 export default Video;
