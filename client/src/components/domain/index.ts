4b13aaac6ab6184fe8152d84aa4b9ca33c023f64
jinhwansuh <nick02190219@gmail.com>
2022-08-14 19:30:57 +0900

    refactor(client): 이름을 받는 auth 페이지에서 컴포넌트로 변경

--- client/src/components/domain/index.ts
+++ client/src/components/domain/index.ts
@@ -3,3 +3,4 @@ export { default as Video } from './Video';
 export { default as ChattingArea } from './ChattingArea';
 export { default as RoomList } from './RoomList';
 export { default as StreamingRoomItem } from './StreamingRoomItem';
+export { default as AuthForm } from './AuthForm';
